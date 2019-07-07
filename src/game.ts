40c8d6ee9e626e8ad915e25604c40461a626ca28
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-07-08 02:00:31 +0300

    #GCARS-3: Разработать базовое управление машиной

---
+++ src/game.ts
@@ -0,0 +1,72 @@
+import 'pixi';
+import 'p2';
+import * as PhaserCE from 'phaser-ce';
+import {Player} from "./player.ts";
+
+declare const URL: string;
+
+class GcarsGame {
+
+    private game: PhaserCE.Game = null;
+    private websocket: WebSocket = null;
+    private currentPlayer: Player = null;
+
+    constructor(width:number = window.innerWidth,
+                height:number = window.innerHeight,
+                renderer:number = PhaserCE.WEBGL) {
+        this.game = new PhaserCE.Game(width, height, renderer, parent);
+        var activeState = {
+            preload: this.preload.bind(this),
+            create: this.create.bind(this),
+            update: this.update.bind(this),
+            render: this.render.bind(this)
+        };
+        this.game.state.add('active', activeState);
+        this.game.state.start('active');
+
+    }
+
+    preload() {
+        this.game.load.image('background', 'assets/concrete1w.jpg');
+        this.game.load.image('car', 'assets/car.png');
+    }
+
+    create() {
+        this.game.add.tileSprite(0, 0, 2000, 2000, 'background');
+        this.game.world.setBounds(0, 0, 2000, 2000);
+        this.websocket = new WebSocket(URL);
+        this.currentPlayer = new Player(
+            this.game.world.randomX,
+            this.game.world.randomY,
+            this.game,
+            'car',
+            80,
+            160,
+            this.websocket,
+            true);
+        this.websocket.onmessage = (em) => {
+            var data = JSON.parse(em.data)
+            this.currentPlayer.moveTo(data.x, data.y, data.angle);
+        };
+        this.websocket.onclose = (em) => {
+            console.log('Connection closed');
+        };
+        this.websocket.onopen = (em) => {
+            console.log('Connection opened');
+        };
+        console.log('Connection created');
+    }
+
+    update() {
+        this.currentPlayer.update();
+    }
+
+    render() {
+        //this.game.debug.text(" Player x = " + this.currentPlayer.x() + "y = " + this.currentPlayer.y(), 200, 200);
+    }
+
+}
+
+window.onload = () => {
+    const game = new GcarsGame(window.innerWidth, window.innerHeight, PhaserCE.WEBGL);
+};
---
+++ src/player.ts
@@ -0,0 +1,53 @@
+import * as PhaserCE from 'phaser-ce';
+
+export class Player {
+
+    protected game: PhaserCE.Game = null;
+    protected playerSprite: PhaserCE.Sprite = null;
+    protected websocket: WebSocket = null;
+    protected wasd = null;
+    protected receivedX: number = 0;
+    protected receivedY: number = 0;
+    protected receivedAngle: number = 0;
+
+    constructor(x: number,
+                y: number,
+                game: PhaserCE.Game,
+                spriteName: string,
+                height: number,
+                width: number,
+                websocket: WebSocket,
+                cameraFollow: boolean) {
+        this.game = game;
+        this.playerSprite = this.game.add.sprite(x, y, spriteName);
+        this.playerSprite.width = width;
+        this.playerSprite.height = height;
+        this.playerSprite.anchor.setTo(0.5, 0.5);
+        this.playerSprite.rotation = -1.57;
+        if (cameraFollow) {
+            this.websocket = websocket;
+            this.game.camera.follow(this.playerSprite);
+            this.wasd = {
+                up: this.game.input.keyboard.addKey(PhaserCE.Keyboard.W),
+                left: this.game.input.keyboard.addKey(PhaserCE.Keyboard.A),
+                down: this.game.input.keyboard.addKey(PhaserCE.Keyboard.S),
+                right: this.game.input.keyboard.addKey(PhaserCE.Keyboard.D)
+            };
+        }
+    }
+
+    public update() {
+        this.playerSprite.x = this.receivedX;
+        this.playerSprite.y = this.receivedY;
+        this.playerSprite.rotation = this.receivedAngle;
+
+    }
+
+    public moveTo(x:number, y:number, angle:number) {
+        this.receivedX = x;
+        this.receivedY = y;
+        this.receivedAngle = angle;
+    }
+
+
+}
