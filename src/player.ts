7f8860329f6820b4e5ce812883117b8a215d5002
Ivan Timofeev <kiberaction@yandex.ru>
2019-09-28 04:56:37 +0300

    broken commit

--- src/player.ts
+++ src/player.ts
@@ -6,9 +6,13 @@ export class Player {
     protected playerSprite: PhaserCE.Sprite = null;
     protected websocket: WebSocket = null;
     protected wasd = null;
+    protected gamepad: PhaserCE.Gamepad;
     protected receivedX: number = 0;
     protected receivedY: number = 0;
     protected receivedAngle: number = 0;
+    protected engineForceLine: PhaserCE.Line;
+    protected forcePointLine: PhaserCE.Line;
+    protected angle: number;
 
     constructor(x: number,
                 y: number,
@@ -18,7 +22,10 @@ export class Player {
                 width: number,
                 websocket: WebSocket,
                 cameraFollow: boolean) {
+        this.angle = 0;
         this.game = game;
+        this.gamepad = game.input.gamepad;
+        this.gamepad.start();
         this.playerSprite = this.game.add.sprite(x, y, spriteName);
         this.playerSprite.width = width;
         this.playerSprite.height = height;
@@ -32,13 +39,27 @@ export class Player {
                 down: this.game.input.keyboard.addKey(PhaserCE.Keyboard.S),
                 right: this.game.input.keyboard.addKey(PhaserCE.Keyboard.D)
             };
+
         }
     }
 
     public update() {
+        if (this.game.input.gamepad.supported && this.game.input.gamepad.active) {
+            var xboxStickLeftX: number = this.gamepad.pad1.axis(PhaserCE.Gamepad.XBOX360_STICK_LEFT_X);
+            var xboxStickLeftY: number = this.gamepad.pad1.axis(PhaserCE.Gamepad.XBOX360_STICK_LEFT_Y);
+            if (xboxStickLeftX < -0.1 && xboxStickLeftX > 0.1 && xboxStickLeftY < -0.1 && xboxStickLeftY > 0.1) {
+                var distance: number = Math.sqrt(xboxStickLeftX^2+xboxStickLeftY^2)   
+                this.angle = Math.asin(xboxStickLeftY / distance)
+            }
+            if (this.gamepad.justPressed(Phaser.Gamepad.XBOX360_A))
+            {
+                
+            }
+            //this.websocket.send(JSON.stringify({angle: this.angle}))
+        }
         this.playerSprite.x = this.receivedX;
         this.playerSprite.y = this.receivedY;
-        this.playerSprite.rotation = this.receivedAngle;
+        this.playerSprite.rotation = this.receivedAngle + Math.PI / 2;
 
     }
 
